acb22f6f7765130dacadd57b4cd96c701700d45b
ArkhamCookie <arkhamcookie@proton.me>
2023-05-14 19:46:22 -0500

    trying out different stylings for boxen adding a error message using boxen (maybe)

--- tests/cliboxTesting.js
+++ tests/cliboxTesting.js
@@ -1,5 +1,37 @@
 import boxen from 'boxen'
+import chalk from 'chalk'
 
-// example boxStyle config
+function boxStyle(text, boxenConfig) {
+	if (text === undefined) {
+		// console.error(chalk.red('boxStyle requires text'))
+		console.error(
+			boxen('boxStyle()\nrequires text', {
+				borderColor: 'redBright',
+				borderStyle: 'bold',
+				title: 'ERROR',
+				titleAlignment: 'right',
+				padding: 0.5
+			})
+		)
+		process.exit(1)
+	}
+	console.log(text, boxenConfig)
+	console.log(boxen(text, boxenConfig))
+}
 
-console.log(boxen('Unicorns', { padding: 1 }))
+// boxStyle()
+// boxStyle('text', "{ borderStyle: 'singleDouble' }")
+
+function boxError(_err) {
+	console.error(
+		boxen(_err, {
+			borderColor: 'redBright',
+			borderStyle: 'bold',
+			title: 'ERROR',
+			titleAlignment: 'right',
+			padding: 0.5
+		})
+	)
+}
+
+boxError('testing')
