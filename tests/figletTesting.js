aa1606dcd216a39fee64f331c924253386e567f7
ArkhamCookie <arkhamcookie@proton.me>
2023-05-14 23:28:44 -0500

    changed figletTesting to export tests

--- tests/figletTesting.js
+++ tests/figletTesting.js
@@ -1,24 +1,29 @@
 import figlet from 'figlet'
 
-figlet('ginit', function(err, data) {
-	if (err) {
-		console.log('Something went wrong, what did you do?')
-		console.dir(err)
-		return
-	}
-	console.log(data)
+function figletBasic(text) {
+	figlet(text, function(err, data) {
+		if (err) {
+			console.log('Something went wrong, what did you do?')
+			console.dir(err)
+			return
+		}
+		console.log(data)
 })
+}
 
-figlet.metadata('Standard', function(err, options, headerComment) {
-	if (err) {
-		console.log('Something went wrong, what did you do?')
-		console.dir(err)
-		return
-	}
-	console.dir(options)
-	console.log(headerComment)
-})
+function figletPrintMetadata() {
+	figlet.metadata('Standard', function(err, options, headerComment) {
+		if (err) {
+			console.log('Something went wrong, what did you do?')
+			console.dir(err)
+			return
+		}
+		console.dir(options)
+		console.log(headerComment)
+	})
+}
 
+function figletPrintFonts() {
 figlet.fonts(function(err, fonts) {
 	if (err) {
 		console.log('Something went wrong, what did you do?')
@@ -27,3 +32,6 @@ figlet.fonts(function(err, fonts) {
 	}
 	console.log(fonts)
 })
+}
+
+export { figletBasic, figletPrintMetadata, figletPrintFonts }
