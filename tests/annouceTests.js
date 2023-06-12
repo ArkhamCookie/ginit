4afbc8f3efdbfc4d707826b6018938249d284106
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-06-12 15:47:54 -0500

    started testing octokit is planned to be an external package (so it can be optional) there isn't a reason for including it if the user doesn't use GitHub

--- tests/annouceTests.js
+++ tests/annouceTests.js
@@ -1,3 +1,7 @@
 import { ginitAnnounce } from '../lib/announce.js'
+import { ginitOctokit, octokitGreet } from '../ext/ginit-octokit/index.js'
 
 ginitAnnounce(true, true)
+
+const octokit = ginitOctokit
+octokitGreet(octokit)
--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,15 @@
-import process from 'node:process'
+// import process from 'node:process'
 import chalk from 'chalk'
 import { ginitAnnounce } from '../lib/announce.js'
+import { ginitOctokit, octokitGreet } from '../ext/ginit-octokit/index.js'
 
 process.on('exit', (code) => {
-	if (code === 1) {
+	if (code === 10) {
 		console.warn(chalk.bold.red('Git repository already exists.'))
 	}
 })
 
-ginitAnnounce(true, true)
+ginitAnnounce(true, false)
+
+const octokit = ginitOctokit
+octokitGreet(octokit)
